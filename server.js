ac8ec3b591132bf1ad2d1cef3b535ce743e951cb
PranavRudra <pranav1024@gmail.com>
2017-11-11 15:36:58 -0500

    Added groupID code

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var bodyParser = require('body-parser')
 
 server.get('/', (req, res) => {res.render('main');});
 
+var groupID = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6)
+
 server.get('/interface', (req,res) => {
     res.render('interface', {
         song: 'a song',
         mood: 'hyped',
+        groupID: groupID,
     });
 });
+
+server.get('/code', (req,res) => {
+    res.send(groupID);
+})
 
 server.listen(3000, () => {console.log('listening on *:3000');});
 

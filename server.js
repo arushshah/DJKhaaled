c284f49a08c498b7d3ca842c797032ec562da1c4
PranavRudra <pranav1024@gmail.com>
2017-11-11 16:08:45 -0500

    Added additional endpoints

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 const server = require('express')();
 const exphbs = require('express-handlebars');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
+//Middleware for parsing incoming requests
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+//Main page endpoint
 server.get('/', (req, res) => {res.render('main');});
 
+//Random groupID
 const groupID = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6)
 
+//User-facing web interface endpoint
 server.get('/interface', (req,res) => {
     res.render('interface', {
         song: 'a song',
@@ -16,15 +24,28 @@ server.get('/interface', (req,res) => {
     });
 });
 
+//GET endpoint for groupID
 server.get('/code', (req,res) => {
     res.send(groupID);
 });
 
-server.listen(3000, () => {console.log('listening on *:3000');});
+//POST endpoint for sensor data 
+//Should be in form {username: data}
+server.post('/sensordata', (req,res) => {
+    console.log(req.body);
+});
 
-//Middleware for parsing incoming requests
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+//POST endpoint to initialize file for user
+server.post('/initializeFile/:username', (req,res) => {
+    let filePath = path.join(`${__dirname}/userFiles/${req.params.username}.txt`);
+
+    //Initializes username.txt file in the userFiles directory
+    fs.writeFileSync(filePath,'', err => {
+        if (err) {throw err;}
+    });
+});
+
+server.listen(3000, () => {console.log('listening on *:3000');});
 
 server.set('views', path.join(__dirname, 'views'));
 server.engine('handlebars', exphbs({defaultLayout:'layout'}));
